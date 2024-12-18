a19040e167e24f4c1b2974aa092598876842624b
Swifter Spaan <66500938+SwifterTheDragon@users.noreply.github.com>
2024-12-18 03:37:25 -0500

    Introduce VerboseSourceRevisionIdGenerator script

---
+++ src/SwifterTheDragon.VerboseSourceRevisionIdBuilder.SourceGenerator/Core/VerboseSourceRevisionIdGenerator.cs
@@ -0,0 +1,84 @@
+// Copyright SwifterTheDragon, 2024. All Rights Reserved.
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using System.Text;
+
+namespace SwifterTheDragon.VerboseSourceRevisionIdBuilder.SourceGenerator.Core
+{
+    /// <summary>
+    /// Generates a single file providing the current verbose source revision ID.
+    /// </summary>
+    [Generator]
+    internal class VerboseSourceRevisionIdGenerator : IIncrementalGenerator
+    {
+        /// <summary>
+        /// The default name of the generated file containing the verbose source revision ID.
+        /// </summary>
+        private string DefaultGeneratedFileName
+        {
+            get
+            {
+                return "VerboseSourceRevisionIdBuilder.Generated.cs";
+            }
+        }
+        /// <summary>
+        /// The default name of the generated namespace containing the verbose source revision ID.
+        /// </summary>
+        private string DefaultGeneratedNamespace
+        {
+            get
+            {
+                return "SwifterTheDragon.VerboseSourceRevisionIdBuilder.SourceGenerator.Core";
+            }
+        }
+        /// <summary>
+        /// The default name of the generated class containing the verbose source revision ID.
+        /// </summary>
+        private string DefaultGeneratedClassName
+        {
+            get
+            {
+                return "VerboseSourceRevisionIdBuilder";
+            }
+        }
+        /// <summary>
+        /// The default name of the generated field containing the verbose source revision ID.
+        /// </summary>
+        private string DefaultFieldName
+        {
+            get
+            {
+                return "VerboseSourceRevisionId";
+            }
+        }
+        public void Initialize(
+            IncrementalGeneratorInitializationContext context)
+        {
+            context.RegisterPostInitializationOutput(
+                callback: postInitializationContext =>
+            {
+                postInitializationContext.AddSource(
+                    hintName: DefaultGeneratedFileName,
+                    sourceText: SourceText.From(
+                        text: @"// Copyright SwifterTheDragon, 2024. All Rights Reserved.
+
+namespace "
+                            + DefaultGeneratedNamespace
+                            + @"
+{
+    internal static class "
+                            + DefaultGeneratedClassName
+                            + @"
+    {
+        internal const string "
+                            + DefaultFieldName
+                            + @" = ""1.2.3"";
+    }
+}
+",
+                        encoding: Encoding.UTF8));
+            });
+        }
+    }
+}
